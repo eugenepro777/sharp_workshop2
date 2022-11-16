3e758d53b93c9554080b902e19e95684c208ed21
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-16 16:34:03 +0300

    Initial commit

---
+++ Task/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа
+
+int randomNum = new Random().Next(100, 1000);
+Console.WriteLine(randomNum);
+
+int num =(randomNum / 100) * 10 + randomNum % 10;
+Console.WriteLine(num);
---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать
+// на вход два числа и выводить, является ли второе
+// число кратным первому. Если число 2 не кратно
+// числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+if(numOne % numTwo == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {numOne % numTwo}");
+}
